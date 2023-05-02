850b87e531e920d61c244ffd69f1ac12731e13d5
Sourabh Kumar <createownpath111@gmail.com>
2023-05-02 13:58:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
     //Write your code here
-  
-  
+  let table = document.getElementById("sampleTable");
+  let newRaw = table.insertRow(0);	
+
+  let leftcell = newRaw.insertCell(0);
+  let rightcell = newRaw.insertCell(1);	
+
+  leftcell.innerText = "New Cell1";
+  rightcell.innerText	= "New Cell2";
 }
